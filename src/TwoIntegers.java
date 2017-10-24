7a1fadf0b69099def3425c081d1ee80b3a7bd7af
bob <bob@APCSA>
2017-10-24 14:06:19 -0700

    finished if else statement.

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -78,5 +78,16 @@ public class TwoIntegers
 			comparingIntegers = num1 + "<" + num2;
 		}
 		return comparingIntegers;
+	public boolean isMultiple()
+		{
+			if (num1 % num2==0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
 }
